738d80597979a434cd29f55c313146453cd35840
seaking7 <seaking7@gmail.com>
2021-04-22 18:29:13 +0900

    greeting 메시지 출력

---
+++ src/main/java/com/uplus/scuserservice/vo/Greeting.java
@@ -0,0 +1,14 @@
+package com.uplus.scuserservice.vo;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+public class Greeting {
+
+    @Value("${greeting.message}")
+    private String message;
+
+}
